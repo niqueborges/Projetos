b8878776a90a24b3659720b85a3784e82f7a3b17
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2023-02-04 15:34:13 -0300

    correção para python3

--- Estudos_Complementares/pesquisa-binaria1.py
+++ Estudos_Complementares/pesquisa-binaria1.py
@@ -3,7 +3,7 @@ def pesquisa_binaria(lista, item):
     alto = len(lista) - 1
 
     while baixo <= alto:
-        meio = (baixo + alto) / 2
+        meio = (baixo + alto) // 2
         chute = lista[meio]
         if chute == item:
             return meio
@@ -15,4 +15,4 @@ def pesquisa_binaria(lista, item):
 minha_lista = [1, 3, 5, 7, 9]
 
 print (pesquisa_binaria(minha_lista, 3)) # => 1
-print (pesquisa_binaria(minha_lista, -1))# => None
+print (pesquisa_binaria(minha_lista, -1)) # => None
