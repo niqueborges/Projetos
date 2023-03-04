25a58a015413f788e155442d088ae8325ee79ada
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2023-03-04 13:43:26 -0300

    up

---
+++ Entradaesaidadedados.py
@@ -0,0 +1,12 @@
+# Algoritmo
+# VarNome : caractere
+# escreva("MeuPrograma")
+# escreva("Digite seu nome:")
+# leia(varNome)
+# escreva("1º programa do", varNome)
+
+print("Meu programa")
+print("Digite seu nome:")
+varNome = input()
+
+print("1º programa do", varNome)
