7335b391b8742ea41a59370f625b56a16846b76b
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2023-03-05 10:12:32 -0300

    up

---
+++ Projetos-Python/1a1000.py
@@ -0,0 +1,7 @@
+# Programar em Python, um programa que imprime na tela os números de 1 a 1000.
+
+print("Meu 1º programa em loop")
+varNumero = 1
+while (varNumero <= 1000):
+    print(varNumero)
+    varNumero = varNumero + 1
