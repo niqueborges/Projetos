c5279a1fc2894f489d9777cd23ef3f54f6c6d46e
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2023-03-05 09:19:11 -0300

    Create cinconumerosInt.py

---
+++ cinconumerosInt.py
@@ -0,0 +1,16 @@
+# Programar em python, um programa que lê 5 números inteiros e calcula a média aritmética deles.
+
+print("Digite o 1º número inteiro: ")
+varNumero1 = int(input())
+print("Digite o 2º número inteiro: ")
+varNumero2 = int(input())
+print("Digite o 3º número inteiro: ")
+varNumero3 = int(input())
+print("Digite o 4º número inteiro: ")
+varNumero4 = int(input())
+print("Digite o 5º número inteiro: ")
+varNumero5 = int(input())
+
+varMedia = ((varNumero1 + varNumero2 + varNumero3 + varNumero4 + varNumero5) / 5)
+
+print("A média aritmética dos 5 números inteiros é: ", varMedia)
