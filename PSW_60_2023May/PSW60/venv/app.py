5e63764c2902964fcaa7641ee287dc1b92d4d844
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2023-04-11 12:51:15 -0300

    pws6Python

--- PSW_60_2023May/PSW60/venv/app.py
+++ PSW_60_2023May/PSW60/venv/app.py
@@ -1,7 +1,18 @@
-idade = 37
-nome = "Monique"
-dado = 1
-print(type(dado)) # tipo de dado
+# idade = 37
+# nome = "Monique"
+# dado = 1
+# print(type(dado)) # tipo de dado
 
-dado = False
-print(f"O meu nome é {nome} e minha idade é {idade}") # comando de saída de dados
+# dado = False
+# print(f"O meu nome é {nome} e minha idade é {idade}") # comando de saída de dados
+
+
+#idade = input("Digite a sua idade: ")
+
+#print(f'Olá, a idade digitada é: {idade}')
+
+
+prova1 = int("10")
+prova2 = int("6")
+somatoria = prova1 + prova2
+print(somatoria)
