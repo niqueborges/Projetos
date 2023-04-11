480e0d89f385936f545f37a707bdd0d462a14a09
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2023-04-11 13:00:53 -0300

    update

--- PSW_60_2023May/PSW60/venv/app.py
+++ PSW_60_2023May/PSW60/venv/app.py
@@ -7,12 +7,19 @@
 # print(f"O meu nome é {nome} e minha idade é {idade}") # comando de saída de dados
 
 
-#idade = input("Digite a sua idade: ")
+# idade = input("Digite a sua idade: ")
 
-#print(f'Olá, a idade digitada é: {idade}')
+# print(f'Olá, a idade digitada é: {idade}')
 
 
-prova1 = int("10")
-prova2 = int("6")
-somatoria = prova1 + prova2
-print(somatoria)
+# Conversão de dados
+# prova1 = int("10")
+# prova2 = int("6")
+# somatoria = prova1 + prova2
+# print(somatoria)
+
+# prova1 = int(input("Digite a nota da prova1: "))
+# prova2 = int(input("Digite a nota da prova2: "))
+# somatoria = prova1 + prova2
+# print(somatoria)
+
