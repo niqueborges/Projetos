4076e03b152913760c2c26325265ceba989e7ff1
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2023-07-12 13:55:10 -0300

    Exercícios

---
+++ PythonNaPratica/Exe28.py
@@ -0,0 +1,5 @@
+'''Crie um programa que realiza a contagem de 0 a 20, exibindo apenas os númros pares.'''
+
+    
+for i in range(0, 21, 2):
+    print(i)
---
+++ PythonNaPratica/Exe29.py
@@ -0,0 +1,7 @@
+'''Crie um programa que realiza a contagem de 0 a 20, exibindo apenas os númros pares.'''
+
+
+for i in range(0, 21):
+if i % 2 == 0:
+    print(i)
+ 
