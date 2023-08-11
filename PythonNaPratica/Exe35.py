70d66c9b91924d84f41d3c470d4f5954d901ed49
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2023-08-11 14:55:22 -0300

    up

--- PythonNaPratica/Exe35.py
+++ PythonNaPratica/Exe35.py
@@ -3,10 +3,17 @@ verifique se esse conteúdo digitado é um palíndromo ou não, exibindo em tela
 
 frase = str(input('Digite uma palavra ou frase: ')).strip().upper()
 palavras = frase.split()
-caracteres = ".join(palavras)fraseinvertida= "
+caracteres = ''.join(palavras)
+fraseinvertida=''
 
-for i in range(len(caracteres)-1, -1, -1): fraseinvertida += caracteres[i]
+for i in range(len(caracteres)-1, -1, -1): 
+    fraseinvertida += caracteres[i]
 
 print(caracteres, fraseinvertida)
+
+if fraseinvertida == caracteres:
+    print('É um palíndromo!!!')
+else:
+    print('Não é um palíndromo...')
     
 
